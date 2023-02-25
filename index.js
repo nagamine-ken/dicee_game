86853ba3af0ac40e8e80df93b8e441ee90b08e51
nagamine-ken <125958860+nagamine-ken@users.noreply.github.com>
2023-02-25 13:31:58 -0800

    dice_game_prototype

---
+++ index.js
@@ -0,0 +1,17 @@
+var randomNumber1 = Math.floor((Math.random() * 6) + 1);
+console.log(randomNumber1);
+
+document.querySelector('img.img1').setAttribute('src', "images/dice" + randomNumber1 + '.png');
+
+var randomNumber2 = Math.floor((Math.random() * 6) + 1);
+console.log(randomNumber2);
+
+document.querySelector('img.img2').setAttribute('src', "images/dice" + randomNumber2 + '.png');
+
+if (randomNumber1 > randomNumber2) {
+    document.querySelector('h1').textContent='Player 1 wins!🏆'
+} else if (randomNumber1 < randomNumber2) {
+    document.querySelector('h1').textContent='Player 2 wins!🏆'
+} else {
+
+}
